05500138ac325e61b401950da7a26674608a7d15
Philippe <philvg@eme2dev.space>
2018-10-30 13:12:34 +0100

    ajout template keys

---
+++ keysTemplate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# Key du serveur Thingspeak
+keyPiscine = key_thingsSpeak
+keyPortail = key_thingsSpeak
+
+# Key du serveur OpenWeatherMap
+meteoKey = key_OpenWeatherMap
+
+# Key du webservice soap CTS
+idCts = id_CTS
+pwdCts =  mdp
+arretCts = code_arret
