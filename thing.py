d344e8edaa12de0d5c05be4ef7d585fb09f52d7a
Philippe <philippe@Phil.local>
2018-11-04 18:54:37 +0100

    gestion erreur thing

--- thing.py
+++ thing.py
@@ -6,29 +6,43 @@ import json
 # Gestion des donnees du site thingspeak
 class Thing:
   def __init__(self, channel, nb=1):
+    
     self.url = "https://api.thingspeak.com/channels/{}/feeds.json?results={}".format(channel, nb)
     self.data = None
+    self.ret = None
     
   # Recuperation de la r�ponse
   def load(self):
     #print("log : load()")
+    headers = { 'Accept':'text/html', 'Accept-Encoding': '', 'User-Agent': None } 
     try:
-        resp = requests.get(self.url, timeout=10)
+        resp = requests.get(self.url, headers= headers, timeout=10)
     except:
+        self.ret = resp.status_code
         return
+    self.ret = resp.status_code
     monJsonUtf = resp.content 
     monJson = monJsonUtf.decode("utf-8") 
  
     self.data = json.loads(monJson)
 
   def getDesc(self):
-    return(self.data['channel'])  
+    if self.ret == 200:
+      return(self.data['channel'])  
+    else:
+      return("")
 
   def getSet(self, offset):
-    return(self.data['feeds'][offset])
+    if self.ret == 200:
+      return(self.data['feeds'][offset])
+    else:
+      return({})
     
   def getField(self, offset, field):
-    return(self.data['feeds'][offset][field])
+    if self.ret == 200:
+      return(self.data['feeds'][offset][field])
+    else:
+      return("")
     
   
     
